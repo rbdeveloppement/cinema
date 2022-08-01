0a67b19b58288058303ed02a0c0cae515625437d
rbdeveloppement <rbdeveloppement@gmail.com>
2022-08-01 10:58:32 +0200

    maj

--- src/helpers/dataManager.helper.js
+++ src/helpers/dataManager.helper.js
@@ -54,62 +54,34 @@ export class DataManager {
     return new ModelClass(jsonRow);
   };
 
+insertOne = (model) => {
+const data = JSON.parse(localStorage.getItem("data-cinema"));
+const table = model.constructor.name.toLowerCase();
+const dataTable = data[table = "data-cinema"];
+const maxId = math.max(...dataTable.map((obj) => obj.id));
+model.id = maxId + 1;
+dataTable.push(model);
+localStorage.setItem("data", JSON.stringify(data));
+console.log(model.constructor.name + " data row inserted", model);
+}
 
-}import * as Models from "../models/index";
+insertMany = (modelsArray) => {
 
-export class DataManager {
-  folder = "data";
-  files = [];
+}
 
-  constructor(files, folder = "data") {
-    this.files = files;
-    this.folder = folder;
-  }
+updateOne = (model) => {
 
-  initDataStorage = async () => {
-    const dataStorage = {};
-    for (const file of this.files) {
-      dataStorage[file] = await this.readJsonFile(file);
-    }
-    localStorage.setItem("data-cinema", JSON.stringify(dataStorage));
-  };
+}  
 
-  readJsonFile = async (fileName) => {
-    let jsonDataArray = [];
+updateMany = (modelsArray) => {
 
-    await fetch(`./src/${this.folder}/${fileName}.json`)
-      .then((resp) => {
-        console.log(resp);
-        return resp.text();
-      })
-      .then((text) => {
-        jsonDataArray = JSON.parse(text);
-      });
+} 
 
-    return jsonDataArray;
-  };
+deleteOne = (model, hard =  false) => {
 
-  getAll = (tableName) => {
-    const allData = JSON.parse(localStorage.getItem("data-cinema"));
-    const jsonTableData = allData[tableName];
-    const modelName = tableName.charAt(0).toUpperCase() + tableName.slice(1);
-    const ModelClass = Models[modelName];
-    return jsonTableData.map((row) => {
-      return new ModelClass(row);
-    });
-  };
+} 
 
-  getOne = (tableName, id) => {
-    const allData = JSON.parse(localStorage.getItem("data-cinema"));
-    const jsonTableData = allData[tableName];
-    const jsonRow = jsonTableData.find((item) => item.id == id);
-    if(!jsonRow){ // jsonRow == undefined
-        return undefined;
-    }
-    const modelName = tableName.charAt(0).toUpperCase() + tableName.slice(1);
-    const ModelClass = Models[modelName];
-    return new ModelClass(jsonRow);
-  };
+deleteMany = (modelsArray, hard = false) => {
 
-
+} 
 }
